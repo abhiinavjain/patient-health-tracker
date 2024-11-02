3711a1815700f7abdad82589017bd5979a693ab9
Abhinav jain <abhinavjain2022@vitbhopal.ac.in>
2024-11-02 22:17:22 +0530

    added about developer and removerd doctor dashboard

--- patient-health-tracker/src/AppRouter.jsx
+++ patient-health-tracker/src/AppRouter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PatientDashboard from './pages/PatientDashboard';
-import DoctorDashboard from './pages/DoctorDashboard';
+
 import FollowUpDashboard from './pages/FollowUpDashboard';
 import Navbar from './pages/NavBar';
 
@@ -12,7 +12,6 @@ const AppRouter = () => {
       <Navbar/>
       <Routes>
         <Route path="/" element={<PatientDashboard />} />
-        <Route path="/doctor" element={<DoctorDashboard />} />
         <Route path="/follow-ups" element={<FollowUpDashboard />} />
       </Routes>
     </Router>
--- patient-health-tracker/src/pages/NavBar.jsx
+++ patient-health-tracker/src/pages/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
+        <navbar fixed="top" className="navbar">
             <div className="navLogo">
                 <h1>Patient Health Tracker</h1>
             </div>
@@ -13,13 +13,13 @@ const Navbar = () => {
                     <Link to="/" className="navlink"><h2>Home</h2></Link>
                 </li>
                 <li>
-                    <Link to="/doctor" className="navlink"><h2>Doctor Dashboard</h2></Link>
-                </li>
-                <li>
                     <Link to="/follow-ups" className="navlink"><h2>Book Appointment</h2></Link>
                 </li>
+                <li>
+                    <Link to={"https://abhiinavjain-portfolio.vercel.app/"} target="blank" className="navlink"><h2>About Developer</h2></Link>
+                </li>
             </ul>
-        </nav>
+        </navbar>
     );
 };
 
